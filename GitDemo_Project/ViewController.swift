6d4ab1dec7c4fc89ab4d85a8be28c13bd9fb52e2
Luis Burga Mideros (s5109289) <s5109289@mac38574.mac.bournemouth.ac.uk>
2019-04-01 09:30:29 +0100

    Initial Commit

---
+++ GitDemo_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo_Project
+//
+//  Created by Luis Burga Mideros (s5109289) on 01/04/2019.
+//  Copyright © 2019 Luis Burga Mideros (s5109289). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GitDemo_ProjectTests/GitDemo_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitDemo_ProjectTests.swift
+//  GitDemo_ProjectTests
+//
+//  Created by Luis Burga Mideros (s5109289) on 01/04/2019.
+//  Copyright © 2019 Luis Burga Mideros (s5109289). All rights reserved.
+//
+
+import XCTest
+@testable import GitDemo_Project
+
+class GitDemo_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
